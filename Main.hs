b2c2113564fb8d7449893180c980882d023b622d
KenethSandoval <kenethra.74@gmail.com>
2021-12-10 21:18:53 -0600

    [feat]: sumOfSquares

--- Main.hs
+++ Main.hs
@@ -1,8 +1,12 @@
 sumOfSquares :: [Integer] -> Integer
 sumOfSquares l = sum (map (^2) l)
 
+sumOfSquares2 :: [Integer] -> Integer
+sumOfSquares2 l = [x * x | x <- l]
+
 
 main :: IO()
 main = do
   let result =  sumOfSquares [1, 2, 3]
-  print result
+  let result2 =  sumOfSquares2 [1, 2, 3]
+  print result2
