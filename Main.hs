9f05a4ee64790d2841a00455a1dd25b2e5e50142
KenethSandoval <kenethra.74@gmail.com>
2021-12-10 22:00:01 -0600

    [feat]: absolute value

--- Main.hs
+++ Main.hs
@@ -1,12 +1,22 @@
 sumOfSquares :: [Integer] -> Integer
-sumOfSquares l = sum (map (^2) l)
+sumOfSquares l = sum [x * x | x <- l]
 
-sumOfSquares2 :: [Integer] -> Integer
-sumOfSquares2 l = [x * x | x <- l]
+secondDegreeEquations :: Double -> Double -> Double -> [Double]
+secondDegreeEquations a b c = 
+  [(-b+d)/n, (-b-d)/n]
+  where d = sqrt(b*b-4*a*c)
+        n = 2*a
 
+absoluteValue :: (Num a, Ord a) => a -> a
+absoluteValue x | x > 0 = x
+                | otherwise = -x
 
 main :: IO()
 main = do
   let result =  sumOfSquares [1, 2, 3]
-  let result2 =  sumOfSquares2 [1, 2, 3]
-  print result2
+  let resultTest = secondDegreeEquations 1 3 2
+  let resultAbsolute = absoluteValue (-4567899)
+
+  print result
+  print resultTest
+  print resultAbsolute
