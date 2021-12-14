13d222a06a7886f26dbb5d309929c6db3513aed9
KenethSandoval <kenethra.74@gmail.com>
2021-12-14 16:29:45 -0600

    fixed: binomio

--- Limits.hs
+++ Limits.hs
@@ -5,3 +5,8 @@ limit f a =
   head[f x | x <- [1..],
              maximum [abs (f y - f x) | y <- [x+1..x+100]] < a ]
 
+binomialSquared :: Integer -> Integer -> Integer
+binomialSquared x y = 
+  x^2 + 2*(x*y) + y^2
+  print (x ++ "+ ")
+
--- Main.hs
+++ Main.hs
@@ -2,5 +2,6 @@ import Limits
 
 main :: IO()
 main = do
-  let result = limit (\n -> (2*n+1)/(n+5)) 0.001 
-  print result
+  -- let result = limit (\n -> (2*n+1)/(n+5)) 0.001 
+  let result2 = binomialSquared (-2) (-3)
+  print result2
