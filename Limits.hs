825ac5d7964d5688391281c9033c2bebae13204a
KenethSandoval <kenethra.74@gmail.com>
2021-12-14 22:46:46 -0600

    fixed: concatenate text

--- Limits.hs
+++ Limits.hs
@@ -5,8 +5,8 @@ limit f a =
   head[f x | x <- [1..],
              maximum [abs (f y - f x) | y <- [x+1..x+100]] < a ]
 
-binomialSquared :: Integer -> Integer -> Integer
-binomialSquared x y = 
-  x^2 + 2*(x*y) + y^2
-  print (x ++ "+ ")
+binomialSquared ::  (Num a) => a -> a -> a -> a
+binomialSquared x y = x^2 + 2*(x*y) + y^2
 
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
