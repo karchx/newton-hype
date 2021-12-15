a8a1831e0d04894ec10a8507d8d3df640a02829d
KenethSandoval <kenethra.74@gmail.com>
2021-12-14 22:48:17 -0600

    [fixed]: text

--- Main.hs
+++ Main.hs
@@ -1,24 +1,6 @@
 import Limits
 
-secondDegreeEquations :: Double -> Double -> Double -> [Double]
-secondDegreeEquations a b c = 
-  [(-b+d)/n, (-b-d)/n]
-  where d = sqrt(b*b-4*a*c)
-        n = 2*a
-
-absoluteValue :: (Num a, Ord a) => a -> a
-absoluteValue x | x > 0 = x
-                | otherwise = -x
-
-test :: Int -> Int
-test n | n > 0 = (n-1)
-
 main :: IO()
 main = do
-  -- let result = limit (\n -> (2*n+1)/(n+5)) 0.001 
-  let result2 = binomialSquared (-2) (-3)
-  -- let result =  sumOfSquares [1, 2, 3]
-  -- let resultTest = secondDegreeEquations 1 3 2
-  -- let resultAbsolute = absoluteValue (-4567899)
-  -- let result2 = test 100
-  print result2
+  let result = applyTwice (binomialSquared 2 3) 2
+  print result
--- Squares.hs
+++ Squares.hs
@@ -3,7 +3,13 @@ module Squares where
 sumOfSquares :: [Integer] -> Integer
 sumOfSquares l = sum (map (^2) l)
 
---sumOfSquares2 :: [Integer] -> Integer
---sumOfSquares2 l = [x * x | x <- l]
+secondDegreeEquations :: Double -> Double -> Double -> [Double]
+secondDegreeEquations a b c = 
+  [(-b+d)/n, (-b-d)/n]
+  where d = sqrt(b*b-4*a*c)
+        n = 2*a
 
+absoluteValue :: (Num a, Ord a) => a -> a
+absoluteValue x | x > 0 = x
+                | otherwise = -x
 
