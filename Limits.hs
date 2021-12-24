81545cafab3b77b91ef9399e4e5ea2a73a73c183
KenethSandoval <kenethra.74@gmail.com>
2021-12-24 15:55:48 -0600

    feat: Vectors

--- Limits.hs
+++ Limits.hs
@@ -1,4 +1,8 @@
 module Limits where
+import Data.Array
+
+type Vector a = Array Int a
+type Matriz a = Array (Int, Int) a
 
 limit :: (Double -> Double) -> Double -> Double
 limit f a = 
@@ -13,4 +17,8 @@ binomialSquared x y = do
     let xyDouble = 2*(x*y)
     let ySquare = y^2
     print (show xSquaree ++ "x^2 + " ++ show xyDouble ++ "xy + " ++ show ySquare ++ "y^2")
+
+listaVector :: Num a => [a] -> Vector a
+listaVector xs = listArray(1,n) xs
+  where n = length xs
 
