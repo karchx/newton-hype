7dfab205fb52ee0159b813ccbaae252df6104771
karchx <kenethra.74@gmail.com>
2023-03-27 15:24:17 -0600

    switch study

--- Utils.hs
+++ Utils.hs
@@ -34,5 +34,20 @@ initials firstname lastname = [f] ++ "." ++ [l] ++ "."
           (l:_) = lastname
 
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis xs = [bmi w h | (w,h) <- xs]
-    where bmi weight height = weight / height ^ 2
+calcBmis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+head' :: [a] -> a
+head' xs = case xs of [] -> error "head doesn't work empty lists"
+                      (x:_) -> x
+
+describeList :: [a] -> String
+describeList xs = "The list is" ++ what xs
+    where what []  = " empty."
+          what [x] = " a singleton list."
+          what xs  = " a lorge list"
