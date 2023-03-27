c4bf9a45c216c4cb55da7fad40093aad62b4f3fe
karchx <kenethra.74@gmail.com>
2023-03-27 11:22:08 -0600

    healt

--- Utils.hs
+++ Utils.hs
@@ -6,9 +6,33 @@ capital all@(x:_) = "The first letter of " ++ all ++ " is " ++ [x]
 
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
-    | weight / height ^ 2 <= 18.5 = "You are underweight. Are you emo?"
-    | weight / height ^ 2 <= 25.0 = "Supposedly you're normal...I hope you're ugly"
-    | weight / height ^ 2 <= 30.0 = "You are fat! lose some chubby weight"
-    | otherwise                   = "Congratulation, you are a whale!"
+    | bmi <= skinny = "You are underweight. Are you emo?"
+    | bmi <= normal = "Supposedly you're normal...I hope you're ugly"
+    | bmi <= fat    = "You are fat! lose some chubby weight"
+    | otherwise     = "Congratulation, you are a whale!"
+    where bmi = weight / height ^ 2
+          skinny = 18.2
+          normal = 25.0
+          fat = 30.0
 
-    
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+ | a < b      = b
+ | otherwise  = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b  = GT
+    | a == b = EQ
+    | otherwise = LT
+
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ "." ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w,h) <- xs]
+    where bmi weight height = weight / height ^ 2
