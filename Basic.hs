85655c4b8d2e0878b11f3a392c4a640ec4acc569
karchx <kenethra.74@gmail.com>
2023-03-28 08:51:38 -0600

    triangle left

---
+++ Basic.hs
@@ -0,0 +1,23 @@
+type TriangleMaker = Char -> Int -> [String]
+
+topLeftTriangle :: TriangleMaker
+topLeftTriangle c n  = [replicate i c | i <- [n, n-1 .. 1]]
+
+getTriangleType :: IO TriangleMaker
+getTriangleType = do
+  let menu = [topLeftTriangle]
+  putStr $ unlines [
+      "What type of triangle do you want to print? (1,2,3 or 4)",
+      "1) Top Left",
+      "2) Bottom Left",
+      "3) Top Right",
+      "4) Bottom Right"]
+
+  line <- getLine
+  return (menu !! ((read line :: Int) - 1))
+
+main :: IO()
+main = do
+    triangle <- getTriangleType
+    size <- getLine
+    putStr $ unlines $ triangle '*' (read size :: Int)
