23ecc68383e8eec6ced4cce1f4cc0fbabe82ad12
karchx <kenethra.74@gmail.com>
2023-03-28 16:18:13 -0600

    right triangle

--- Basic.hs
+++ Basic.hs
@@ -3,9 +3,18 @@ type TriangleMaker = Char -> Int -> [String]
 topLeftTriangle :: TriangleMaker
 topLeftTriangle c n  = [replicate i c | i <- [n, n-1 .. 1]]
 
+bottomLeftTriangle :: TriangleMaker
+bottomLeftTriangle c n = [replicate i c | i <- [1 .. n]]
+
+topRightTriangle :: TriangleMaker
+topRightTriangle c n  = [(replicate ( n - i) ' ') ++ (replicate i c) | i <- [n, n-1 .. 1]]
+
+bottomRightTriangle :: TriangleMaker
+bottomRightTriangle c n = [(replicate ( n - i) ' ') ++ (replicate i c) | i <- [1 .. n]]
+
 getTriangleType :: IO TriangleMaker
 getTriangleType = do
-  let menu = [topLeftTriangle]
+  let menu = [topLeftTriangle, bottomLeftTriangle, topRightTriangle, bottomRightTriangle]
   putStr $ unlines [
       "What type of triangle do you want to print? (1,2,3 or 4)",
       "1) Top Left",
